2810402178c6fc76f4a02f7abf54592caffde2ef
Fionn Malone <fmalone@google.com>
2022-02-14 19:38:21 +0000

    Remove wrong scf file.

--- pie/qmc/tests/reference_data/neon_cc-pvdz_rhf/scf.py
+++
@@ -1,9 +0,0 @@
-from pyscf import gto, scf, cc
-
-atom = gto.M(atom=[('H', 1.6*i, 0, 0) for i in range(0,10)],
-             basis='sto-6g',
-             verbose=4,
-             unit='Bohr')
-mf = scf.UHF(atom)
-mf.chkfile = 'scf.chk'
-mf.kernel()
