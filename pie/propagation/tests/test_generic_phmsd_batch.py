d1fbe0f702009e8ed8f27be434185d467a8b3616
Fionn Malone <fmalone@google.com>
2022-02-16 20:47:48 +0000

    Add more unit tests.

--- pie/propagation/tests/test_generic_phmsd_batch.py
+++ pie/propagation/tests/test_generic_phmsd_batch.py
@@ -151,6 +151,142 @@ def test_phmsd_overlap_batch():
         assert numpy.allclose(ovlps[iw], ovlps_opt[iw])
 
 @pytest.mark.unit
+def test_get_dets_single_excitation_batched():
+    nwalker = 10
+    ndets = 10
+    nmo = 10
+    G0  = numpy.random.random((nwalker, nmo, nmo)).astype(numpy.complex128)
+    G0 += 1j*numpy.random.random((nwalker, nmo, nmo))
+    cre_a = [3]
+    anh_a = [0]
+    p = cre_a[0]
+    q = anh_a[0]
+    ref = numpy.zeros((nwalker, ndets), dtype=numpy.complex128)
+    for iw in range(nwalker):
+        for idet in range(ndets):
+            ref[iw, idet] = G0[iw, p, q]
+
+    from pie.utils.misc import dotdict
+    trial = dotdict(
+                {
+                'cre_ex_a': [[0], numpy.array([[p]]*ndets, dtype=int)],
+                'anh_ex_a': [[0], numpy.array([[q]]*ndets, dtype=int)],
+                'cre_ex_b': [[0], numpy.array([[p]]*ndets, dtype=int)],
+                'anh_ex_b': [[0], numpy.array([[q]]*ndets, dtype=int)],
+                }
+                )
+    from pie.propagation.overlap import get_dets_single_excitation_batched
+    test = get_dets_single_excitation_batched(G0, G0, trial)
+    assert numpy.allclose(ref, test)
+
+@pytest.mark.unit
+def test_get_dets_double_excitation_batched():
+    nwalker = 10
+    ndets = 10
+    nmo = 10
+    G0  = numpy.random.random((nwalker, nmo, nmo)).astype(numpy.complex128)
+    G0 += 1j*numpy.random.random((nwalker, nmo, nmo))
+    cre_a = [3, 7]
+    anh_a = [0, 2]
+    p = cre_a[0]
+    q = anh_a[0]
+    r = cre_a[1]
+    s = anh_a[1]
+    ref = numpy.zeros((nwalker, ndets), dtype=numpy.complex128)
+    for iw in range(nwalker):
+        for idet in range(ndets):
+            ref[iw, idet] = G0[iw,p,q]*G0[iw,r,s] - G0[iw,p,s]*G0[iw,r,q]
+
+    from pie.utils.misc import dotdict
+    trial = dotdict(
+                {
+                'cre_ex_a': [[0], [0], numpy.array([cre_a]*ndets, dtype=int)],
+                'anh_ex_a': [[0], [0], numpy.array([anh_a]*ndets, dtype=int)],
+                'cre_ex_b': [[0], [0], numpy.array([cre_a]*ndets, dtype=int)],
+                'anh_ex_b': [[0], [0], numpy.array([anh_a]*ndets, dtype=int)],
+                }
+                )
+    from pie.propagation.overlap import get_dets_double_excitation_batched
+    test = get_dets_double_excitation_batched(G0, G0, trial)
+    assert numpy.allclose(ref, test)
+
+@pytest.mark.unit
+def test_get_dets_triple_excitation_batched():
+    nwalker = 10
+    ndets = 10
+    nmo = 10
+    G0  = numpy.random.random((nwalker, nmo, nmo)).astype(numpy.complex128)
+    G0 += 1j*numpy.random.random((nwalker, nmo, nmo))
+    cre_a = [3, 7, 9]
+    anh_a = [0, 1, 2]
+    p = cre_a[0]
+    q = anh_a[0]
+    r = cre_a[1]
+    s = anh_a[1]
+    t = cre_a[2]
+    u = anh_a[2]
+    ref = numpy.zeros((nwalker, ndets), dtype=numpy.complex128)
+    for iw in range(nwalker):
+        for idet in range(ndets):
+            G0a = G0[iw]
+            ovlp_a = G0a[p,q]*(G0a[r,s]*G0a[t,u] - G0a[r,u]*G0a[t,s])
+            ovlp_a -= G0a[p,s]*(G0a[r,q]*G0a[t,u] - G0a[r,u]*G0a[t,q])
+            ovlp_a += G0a[p,u]*(G0a[r,q]*G0a[t,s] - G0a[r,s]*G0a[t,q])
+            ref[iw, idet] = ovlp_a
+
+    from pie.utils.misc import dotdict
+    trial = dotdict(
+                {
+                'cre_ex_a': [[0], [0], [0], numpy.array([cre_a]*ndets, dtype=int)],
+                'anh_ex_a': [[0], [0], [0], numpy.array([anh_a]*ndets, dtype=int)],
+                'cre_ex_b': [[0], [0], [0], numpy.array([cre_a]*ndets, dtype=int)],
+                'anh_ex_b': [[0], [0], [0], numpy.array([anh_a]*ndets, dtype=int)],
+                }
+                )
+    from pie.propagation.overlap import get_dets_triple_excitation_batched
+    test = get_dets_triple_excitation_batched(G0, G0, trial)
+    assert numpy.allclose(ref, test)
+
+@pytest.mark.unit
+def test_get_dets_nfold_excitation_batched():
+    nwalker = 10
+    ndets = 10
+    nmo = 18
+    G0  = numpy.random.random((nwalker, nmo, nmo)).astype(numpy.complex128)
+    G0 += 1j*numpy.random.random((nwalker, nmo, nmo))
+    cre_a = [9, 10, 11, 13, 15]
+    anh_a = [0, 1, 2, 4, 8]
+    ref = numpy.zeros((nwalker, ndets), dtype=numpy.complex128)
+    nex_a = 5
+    for iw in range(nwalker):
+        G0a = G0[iw]
+        for idet in range(ndets):
+            det_a = numpy.zeros((nex_a,nex_a), dtype=numpy.complex128)
+            for iex in range(nex_a):
+                p = cre_a[iex]
+                q = anh_a[iex]
+                det_a[iex,iex] = G0a[p,q]
+                for jex in range(iex+1, nex_a):
+                    r = cre_a[jex]
+                    s = anh_a[jex]
+                    det_a[iex, jex] = G0a[p,s]
+                    det_a[jex, iex] = G0a[r,q]
+            ref[iw, idet] = numpy.linalg.det(det_a)
+    from pie.utils.misc import dotdict
+    empty = [[0], [0], [0], [0], [0]]
+    trial = dotdict(
+                {
+                'cre_ex_a': empty + [numpy.array([cre_a]*ndets, dtype=int)],
+                'anh_ex_a': empty + [numpy.array([anh_a]*ndets, dtype=int)],
+                'cre_ex_b': empty + [numpy.array([cre_a]*ndets, dtype=int)],
+                'anh_ex_b': empty + [numpy.array([anh_a]*ndets, dtype=int)],
+                }
+                )
+    from pie.propagation.overlap import get_dets_nfold_excitation_batched
+    test = get_dets_nfold_excitation_batched(5, G0, G0, trial)
+    assert numpy.allclose(ref, test)
+
+@pytest.mark.unit
 def test_phmsd_propagation_batch():
     numpy.random.seed(7)
     nmo = 10
