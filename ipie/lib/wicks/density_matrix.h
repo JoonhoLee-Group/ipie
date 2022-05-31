620c4cfe193ac149709b0487d031bcb28f2ae541
Fionn Malone <fionn.malone@gmail.com>
2022-05-31 06:33:23 +0000

    Support for complex ci coeffs.

--- ipie/lib/wicks/density_matrix.h
+++ ipie/lib/wicks/density_matrix.h
@@ -1,9 +1,21 @@
 #pragma once
 
+#include <complex.h>
+
 void compute_density_matrix(
     double* ci_coeffs,
     u_int64_t* dets,
     double* density_matrix,
+    int* occs,
+    size_t num_dets,
+    size_t num_orbs,
+    size_t nel
+    );
+
+void compute_density_matrix_cmplx(
+    complex double* ci_coeffs,
+    u_int64_t* dets,
+    complex double* density_matrix,
     int* occs,
     size_t num_dets,
     size_t num_orbs,
@@ -15,6 +27,15 @@ void fill_diagonal_term(
     double ci_coeff,
     int* occs,
     double* density_matrix,
+    size_t num_orbs,
+    size_t nel
+    );
+
+void fill_diagonal_term_cmplx(
+    u_int64_t *det,
+    complex double ci_coeff,
+    int* occs,
+    complex double* density_matrix,
     size_t num_orbs,
     size_t nel
     );
