e9c48f12c017e7659e7c1a71cd5a215b91e25f72
Fionn Malone <fionn.malone@gmail.com>
2022-06-02 18:52:42 +0000

    Add unit tests for active space energy.

--- ipie/estimators/tests/test_generic_phmsd.py
+++ ipie/estimators/tests/test_generic_phmsd.py
@@ -436,8 +436,7 @@ def test_kernels_energy():
     ndets_level = len(trial.cre_ex_b[iexcit])
     det_mat = numpy.zeros((nwalkers, ndets_level, iexcit, iexcit), dtype=numpy.complex128)
     cof_mat = numpy.zeros((nwalkers, ndets_level, iexcit-1, iexcit-1), dtype=numpy.complex128)
-    get_det_matrix_batched(
-            iexcit,
+    wk.build_det_matrix(
             trial.cre_ex_b[iexcit],
             trial.anh_ex_b[iexcit],
             walker_batch.G0b,
@@ -843,6 +842,251 @@ def test_kernels_gf_active_space():
         walker_batch_ref.Gb
         )
     assert numpy.allclose(walker_batch_ref.Ga, walker_batch_test.Ga)
+
+@pytest.mark.unit
+def test_kernels_energy_active_space():
+    numpy.random.seed(7)
+    nmo = 30
+    nelec = (9,9)
+    nwalkers = 1
+    nsteps = 100
+    nact = 12
+    ncore = 2
+    h1e, chol, enuc, eri = generate_hamiltonian(nmo, nelec, cplx=False)
+    system = Generic(nelec=nelec)
+    ham = HamGeneric(h1e=numpy.array([h1e,h1e]),
+                     chol=chol.reshape((-1,nmo*nmo)).T.copy(),
+                     ecore=0, options = {"symmetry":False})
+    wfn, init = get_random_phmsd(7, 7, nact, ndet=5000, init=True)
+    ci, occa, occb = wfn
+    core = [0, 1]
+    with_core_a = [numpy.array(core + [orb + 2 for orb in oa], dtype=numpy.int32) for oa in occa]
+    with_core_b = [numpy.array(core + [orb + 2 for orb in ob], dtype=numpy.int32) for ob in occb]
+    wfn_2 = (ci[::50], with_core_a[::50], with_core_b[::50]) # Get high excitation determinants too
+
+    trial_ref = MultiSlater(
+            system,
+            ham,
+            wfn_2,
+            options={
+                'wicks': True,
+                'optimized': True,
+                'use_wicks_helper': False,
+                }
+            )
+    trial_test = MultiSlater(
+            system,
+            ham,
+            wfn_2,
+            options={
+                'wicks': True,
+                'optimized': True,
+                'use_wicks_helper': False,
+                'nact': 12,
+                'ncas': 14,
+                }
+            )
+
+    numpy.random.seed(7)
+    qmc = dotdict(
+            {'dt': 0.005, 'nstblz': 5, 'batched': True, 'nwalkers': nwalkers}
+            )
+    options = {'hybrid': True}
+    prop = Continuous(system, ham, trial_ref, qmc, options=options)
+    walker_batch_ref = MultiDetTrialWalkerBatch(system, ham, trial_ref, nwalkers)
+    walker_batch_test = MultiDetTrialWalkerBatch(system, ham, trial_test, nwalkers)
+    numpy.random.seed(7)
+    for i in range (nsteps):
+        prop.propagate_walker_batch(walker_batch_ref, system, ham, trial_ref, 0)
+        walker_batch_ref.reortho()
+
+    walker_batch_test.phia = walker_batch_ref.phia.copy()
+    walker_batch_test.phib = walker_batch_ref.phib.copy()
+    greens_function_multi_det_wicks(walker_batch_ref, trial_ref)
+    walker_batch_test.G0a = walker_batch_ref.G0a.copy()
+    walker_batch_test.G0b = walker_batch_ref.G0b.copy()
+    walker_batch_test.Ga = walker_batch_ref.Ga.copy()
+    walker_batch_test.Gb = walker_batch_ref.Gb.copy()
+    walker_batch_test.Ghalfa = walker_batch_ref.Ghalfa.copy()
+    walker_batch_test.Ghalfb = walker_batch_ref.Ghalfb.copy()
+
+    from ipie.estimators.kernels.cpu import wicks as wk
+
+    ndets = trial_ref.ndets
+    nchol = ham.nchol
+    ref = numpy.zeros((nwalkers, ndets, nchol), dtype=numpy.complex128)
+    test = numpy.zeros((nwalkers, ndets, nchol), dtype=numpy.complex128)
+    ovlpa = walker_batch_ref.det_ovlpas
+    ovlpb = walker_batch_ref.det_ovlpbs
+
+    c_phasea_ovlpb = numpy.einsum(
+                        'wJ,J->wJ',
+                        ovlpb,
+                        trial_ref.phase_a*trial_ref.coeffs.conj(),
+                        optimize=True)
+    c_phaseb_ovlpa = numpy.einsum(
+                        'wJ,J->wJ',
+                        ovlpa,
+                        trial_ref.phase_b*trial_ref.coeffs.conj(),
+                        optimize=True)
+    walker_batch_ref.CIa.fill(0.0+0.0j)
+    walker_batch_ref.CIb.fill(0.0+0.0j)
+    from ipie.utils.testing import shaped_normal
+    from ipie.estimators.local_energy_wicks import (
+            build_slices
+            )
+    Lbb = shaped_normal((nwalkers, nmo, system.ndown, nchol))
+    slices_alpha, slices_beta = build_slices(trial_ref)
+    assert trial_ref.nfrozen != trial_test.nfrozen
+    # 1.
+    wk.fill_os_singles(
+            trial_ref.cre_ex_b[1],
+            trial_ref.anh_ex_b[1],
+            trial_ref.nfrozen,
+            Lbb,
+            ref,
+            slices_beta[1]
+            )
+    act_orb = trial_test.act_orb_beta
+    occ_orb = trial_test.occ_orb_beta
+    wk.fill_os_singles(
+            trial_test.cre_ex_b[1],
+            trial_test.anh_ex_b[1],
+            trial_test.nfrozen,
+            Lbb[:,act_orb,occ_orb,:].copy(),
+            test,
+            slices_beta[1]
+            )
+    assert numpy.allclose(ref, test)
+    # 2.
+    G0 = shaped_normal((nwalkers, nmo, nmo), cmplx=True)
+    ref = numpy.zeros((nwalkers, ndets, nchol), dtype=numpy.complex128)
+    test = numpy.zeros((nwalkers, ndets, nchol), dtype=numpy.complex128)
+    wk.fill_os_doubles(
+            trial_ref.cre_ex_b[2],
+            trial_ref.anh_ex_b[2],
+            trial_ref.nfrozen,
+            G0,
+            Lbb,
+            ref,
+            slices_beta[2]
+            )
+    wk.fill_os_doubles(
+            trial_test.cre_ex_b[2],
+            trial_test.anh_ex_b[2],
+            trial_test.nfrozen,
+            G0,
+            Lbb[:,act_orb,occ_orb,:].copy(),
+            test,
+            slices_beta[2]
+            )
+    assert numpy.allclose(ref, test)
+    ref = numpy.zeros((nwalkers, ndets, nchol), dtype=numpy.complex128)
+    test = numpy.zeros((nwalkers, ndets, nchol), dtype=numpy.complex128)
+    iexcit = 3
+    wk.fill_os_triples(
+            trial_ref.cre_ex_b[iexcit],
+            trial_ref.anh_ex_b[iexcit],
+            trial_ref.nfrozen,
+            G0,
+            Lbb,
+            ref,
+            slices_beta[iexcit]
+            )
+    wk.fill_os_triples(
+            trial_test.cre_ex_b[iexcit],
+            trial_test.anh_ex_b[iexcit],
+            trial_test.nfrozen,
+            G0,
+            Lbb[:,act_orb,occ_orb,:].copy(),
+            test,
+            slices_beta[iexcit]
+            )
+    assert numpy.allclose(ref, test)
+    ref = numpy.zeros((nwalkers, ndets), dtype=numpy.complex128)
+    test = numpy.zeros((nwalkers, ndets), dtype=numpy.complex128)
+    iexcit = 2
+    wk.get_ss_doubles(
+            trial_ref.cre_ex_b[iexcit],
+            trial_ref.anh_ex_b[iexcit],
+            Lbb,
+            ref,
+            slices_beta[iexcit]
+            )
+    wk.get_ss_doubles(
+            trial_test.cre_ex_b[iexcit],
+            trial_test.anh_ex_b[iexcit],
+            Lbb[:,act_orb,occ_orb,:].copy(),
+            test,
+            slices_beta[iexcit]
+            )
+    assert numpy.allclose(ref, test)
+    iexcit = 4
+    ndets_level = len(trial_ref.cre_ex_b[iexcit])
+    det_mat_ref = numpy.zeros((nwalkers, ndets_level, iexcit, iexcit), dtype=numpy.complex128)
+    det_mat_test = numpy.zeros((nwalkers, ndets_level, iexcit, iexcit), dtype=numpy.complex128)
+    cof_mat = numpy.zeros((nwalkers, ndets_level, iexcit-1, iexcit-1), dtype=numpy.complex128)
+    wk.build_det_matrix(
+            trial_ref.cre_ex_b[iexcit],
+            trial_ref.anh_ex_b[iexcit],
+            trial_ref.nfrozen,
+            walker_batch_ref.G0b,
+            det_mat_ref)
+    wk.build_det_matrix(
+            trial_test.cre_ex_b[iexcit],
+            trial_test.anh_ex_b[iexcit],
+            trial_test.nfrozen,
+            walker_batch_test.G0b,
+            det_mat_test)
+    assert numpy.allclose(det_mat_test, det_mat_ref)
+    ref = numpy.zeros((nwalkers, ndets, nchol), dtype=numpy.complex128)
+    test = numpy.zeros((nwalkers, ndets, nchol), dtype=numpy.complex128)
+    from ipie.estimators.local_energy_wicks import fill_opp_spin_factors_batched_chol
+    cof_mat = numpy.zeros((nwalkers, ndets_level, iexcit-1, iexcit-1), dtype=numpy.complex128)
+    wk.fill_os_nfold(
+            trial_ref.cre_ex_b[iexcit],
+            trial_ref.anh_ex_b[iexcit],
+            det_mat_ref,
+            cof_mat,
+            Lbb,
+            ref,
+            slices_beta[iexcit]
+            )
+    cof_mat = numpy.zeros((nwalkers, ndets_level, iexcit-1, iexcit-1), dtype=numpy.complex128)
+    wk.fill_os_nfold(
+            trial_test.cre_ex_b[iexcit],
+            trial_test.anh_ex_b[iexcit],
+            det_mat_test,
+            cof_mat,
+            Lbb[:,act_orb,occ_orb,:].copy(),
+            test,
+            slices_beta[iexcit]
+            )
+    assert numpy.allclose(ref, test)
+    ref = numpy.zeros((nwalkers, ndets), dtype=numpy.complex128)
+    test = numpy.zeros((nwalkers, ndets), dtype=numpy.complex128)
+    from ipie.estimators.local_energy_wicks import fill_same_spin_contribution_batched_contr
+    cof_mat = numpy.zeros((nwalkers, ndets_level, iexcit-1, iexcit-1), dtype=numpy.complex128)
+    wk.get_ss_nfold(
+            trial_ref.cre_ex_b[iexcit],
+            trial_ref.anh_ex_b[iexcit],
+            det_mat_ref,
+            cof_mat,
+            Lbb,
+            ref,
+            slices_beta[iexcit]
+            )
+    cof_mat = numpy.zeros((nwalkers, ndets_level, iexcit-1, iexcit-1), dtype=numpy.complex128)
+    wk.get_ss_nfold(
+            trial_test.cre_ex_b[iexcit],
+            trial_test.anh_ex_b[iexcit],
+            det_mat_test,
+            cof_mat,
+            Lbb[:,act_orb,occ_orb,:].copy(),
+            test,
+            slices_beta[iexcit]
+            )
+    assert numpy.allclose(ref, test)
 
 if __name__ == '__main__':
     test_phmsd_local_energy()
