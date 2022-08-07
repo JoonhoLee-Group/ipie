6cbaee3f36563735659031d6a0a5bdafe74fcde8
Fionn Malone <fmalone@google.com>
2022-08-07 04:23:45 +0000

    Update base class.

--- ipie/estimators/estimator_base.py
+++ ipie/estimators/estimator_base.py
@@ -24,7 +24,15 @@ class EstimatorBase(object):
     @property
     def ascii_filename(self) -> str:
         """Text file for output"""
-        self._ascii_filename
+        return self._ascii_filename
+
+    @ascii_filename.setter
+    def ascii_filename(self, name):
+        """Text file for output"""
+        self._ascii_filename = name
+        if self._ascii_filename is not None:
+            with open(self._ascii_filename, 'w') as f:
+                f.write(self.header_to_text + '\n')
 
     @property
     def write_frequency(self) -> str:
@@ -35,6 +43,11 @@ class EstimatorBase(object):
     def shape(self) -> tuple:
         """Shape of estimator."""
         return self._shape
+
+    @property
+    def size(self) -> int:
+        """Shape of estimator."""
+        return np.prod(self._shape)
 
     @shape.setter
     def shape(self, shape) -> tuple:
@@ -58,9 +71,32 @@ class EstimatorBase(object):
         return format_fixed_width_strings(self.names)
 
     def data_to_text(self, vals) -> str:
-        vals
         assert len(vals) == len(self.names)
         return format_fixed_width_floats(vals.real)
+
+    def to_ascii_file(self, vals: str) -> None:
+        if self.ascii_filename is not None:
+            with open(self._ascii_filename, 'a') as f:
+                f.write(vals + '\n')
+
+    def __getitem__(self, name):
+        data = self._data.get(name)
+        if data is None:
+            raise RuntimeError(f"Unknown estimator {name}")
+        return self._data[name]
+
+    def __setitem__(self, name, val):
+        data = self._data.get(name)
+        if data is None:
+            raise RuntimeError(f"Unknown estimator {name}")
+        self._data[name] = val
+
+    def zero(self):
+        for k, v in self._data.items():
+            if isinstance(v, np.ndarray):
+                self._data[k] = np.zeros_like(v)
+            else:
+                self._data[k] = 0.0j
 
     def post_reduce_hook(self) -> None:
         pass
