af04bb9725d1f981710bd414086db4e9ed864d7d
Fionn Malone <fmalone@google.com>
2022-10-03 21:12:21 +0000

    Finish overlap kernels.

--- ipie/estimators/kernels/__init__.py
+++ ipie/estimators/kernels/__init__.py
@@ -2,7 +2,7 @@
 from ipie.config import config
 if config.get_option('use_gpu'):
     from .gpu.exchange import exchange_reduction
-    import .gpu.wicks as wicks
+    from .gpu import wicks
 else:
     exchange_reduction = None
-    import .cpu.wicks as wicks
+    from .cpu import wicks
