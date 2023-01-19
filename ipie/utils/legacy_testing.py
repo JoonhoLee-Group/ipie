76cc17f73b63b62b220407dcfee992ee4dd7789b
Fionn Malone <fionn.malone@gmail.com>
2023-01-19 08:56:05 -0800

    More testing utility for legacy.

--- ipie/utils/legacy_testing.py
+++ ipie/utils/legacy_testing.py
@@ -1,14 +1,32 @@
 import copy
+from typing import Tuple, Union
+import numpy as np
 
 from ipie.legacy.propagation.continuous import Continuous as LegacyContinuous
 from ipie.legacy.walkers.single_det import SingleDetWalker
 from ipie.legacy.trial_wavefunction.multi_slater import MultiSlater
+from ipie.legacy.walkers.handler import Walkers
 from ipie.legacy.estimators.local_energy import local_energy_generic_cholesky_opt
+
+from ipie.utils.testing import (
+    generate_hamiltonian,
+    get_random_nomsd,
+    get_random_phmsd_opt,
+)
 from ipie.utils.misc import dotdict
+from ipie.hamiltonians.generic import Generic as HamGeneric
+from ipie.systems.generic import Generic
 
 
 def build_legacy_test_case(
-    wfn, init, system, ham, num_steps, num_walkers, dt, nstblz=5
+    wfn,
+    init,
+    system,
+    ham,
+    num_steps,
+    num_walkers,
+    dt,
+    nstblz=5,
 ):
     qmc = dotdict({"dt": dt, "nstblz": nstblz})
     options = {"hybrid": True}
@@ -48,3 +66,126 @@ def get_legacy_walker_energies(system, ham, trial, walkers):
         e1s += [e[1]]
         e2s += [e[2]]
     return etots, e1s, e2s
+
+
+def build_legacy_test_case_handlers_mpi(
+    num_elec: Tuple[int, int],
+    num_basis: int,
+    num_dets=1,
+    wfn_type="phmsd",
+    complex_integrals: bool = False,
+    complex_trial: bool = False,
+    seed: Union[int, None] = None,
+    options={},
+):
+    if seed is not None:
+        np.random.seed(seed)
+    assert len(options) > 0
+    h1e, chol, enuc, eri = generate_hamiltonian(
+        num_basis, num_elec, cplx=complex_integrals
+    )
+    system = Generic(nelec=num_elec)
+    ham_legacy = HamGeneric(
+        h1e=np.array([h1e, h1e]),
+        chol=chol.reshape((-1, num_basis**2)).T.copy(),
+        ecore=0,
+        options={"symmetry": False},
+    )
+    ham_legacy.control_variate = False
+
+    if wfn_type == "phmsd":
+        wfn, init = get_random_phmsd_opt(
+            num_elec[0],
+            num_elec[1],
+            num_basis,
+            ndet=num_dets,
+            init=True,
+            cmplx_coeffs=complex_trial,
+        )
+    else:
+        coeffs, wfn, init = get_random_nomsd(
+            num_elec[0], num_elec[1], num_basis, num_dets, cplx=complex_trial, init=True
+        )
+
+    trial = MultiSlater(system, ham_legacy, wfn, init=init)
+    trial.half_rotate(system, ham_legacy)
+    trial.calculate_energy(system, ham_legacy)
+    if trial.ndets == 1:
+        trial.psi = trial.psi[0]
+    # necessary for backwards compatabilty with tests
+    if seed is not None:
+        np.random.seed(seed)
+    prop = LegacyContinuous(system, ham_legacy, trial, options, options=options)
+    handler = Walkers(
+        system,
+        ham_legacy,
+        trial,
+        options,
+        options,
+        verbose=False,
+        comm=options.mpi_handler.comm,
+    )
+    for i in range(options.num_steps):
+        for walker in handler.walkers:
+            prop.propagate_walker(walker, system, ham_legacy, trial, 0.0)
+            _ = walker.reortho(trial)  # reorthogonalizing to stablize
+            walker.greens_function(trial)
+        if options.comm is not None:
+            handler.pop_control(options.mpi_handler)
+    return handler
+
+
+def build_legacy_test_case_handlers(
+    num_elec: Tuple[int, int],
+    num_basis: int,
+    num_dets=1,
+    wfn_type="phmsd",
+    complex_integrals: bool = False,
+    complex_trial: bool = False,
+    seed_ham: bool = None,
+    seed_wavefunction: bool = None,
+    options={},
+):
+    assert len(options) > 0
+    h1e, chol, enuc, eri = generate_hamiltonian(
+        num_basis, num_elec, cplx=complex_integrals
+    )
+    system = Generic(nelec=num_elec)
+    ham_legacy = HamGeneric(
+        h1e=np.array([h1e, h1e]),
+        chol=chol.reshape((-1, num_basis**2)).T.copy(),
+        ecore=0,
+        options={"symmetry": False},
+    )
+    ham_legacy.control_variate = False
+
+    if wfn_type == "phmsd":
+        wfn, init = get_random_phmsd_opt(
+            num_elec[0],
+            num_elec[1],
+            num_basis,
+            ndet=num_dets,
+            init=True,
+            cmplx_coeffs=complex_trial,
+        )
+    else:
+        coeffs, wfn, init = get_random_nomsd(
+            num_elec[0], num_elec[1], num_basis, num_dets, cplx=complex_trial, init=True
+        )
+
+    trial = MultiSlater(system, ham_legacy, wfn, init=init)
+    trial.half_rotate(system, ham_legacy)
+    if trial.ndets == 1:
+        trial.psi = trial.psi[0]
+    prop = LegacyContinuous(system, ham_legacy, trial, options, options=options)
+    handler = Walkers(
+        system, ham_legacy, trial, options, options, verbose=False, comm=options.comm
+    )
+    for i in range(num_steps):
+        for walker in handler.walkers:
+            prop.propagate_walker(walker, system, ham_legacy, trial, 0.0)
+            _ = walker.reortho(trial)  # reorthogonalizing to stablize
+            walker.greens_function(trial)
+    #        if options.comm is not None:
+    #            handler.pop_control(options.comm)
+    return handler
