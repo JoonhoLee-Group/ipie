Add UEG calculation as example. Fixed trial energy calculation for complex integrals.

ipie/utils/testing.py
@@ -21,6 +21,8 @@ from dataclasses import dataclass
 from typing import Tuple, Union
 
 import numpy
+import scipy.sparse
+import scipy.stats
 
 from ipie.hamiltonians import Generic as HamGeneric
 from ipie.propagation.phaseless_generic import PhaselessBase, PhaselessGeneric
@@ -44,13 +46,28 @@ from ipie.walkers.pop_controller import PopController
 from ipie.walkers.walkers_dispatch import UHFWalkersTrial
 
 
-def generate_hamiltonian(nmo, nelec, cplx=False, sym=8, tol=1e-3):
-    h1e = numpy.random.random((nmo, nmo))
-    if cplx:
-        h1e = h1e + 1j * numpy.random.random((nmo, nmo))
-    eri = numpy.random.normal(scale=0.01, size=(nmo, nmo, nmo, nmo))
-    if cplx:
-        eri = eri + 1j * numpy.random.normal(scale=0.01, size=(nmo, nmo, nmo, nmo))
+def generate_hamiltonian(nmo, nelec, cplx=False, sym=8, sparse=False, tol=1e-3):
+    h1e, eri = None, None
+    if sparse:
+        h1e = scipy.sparse.random(nmo, nmo).toarray()
+        if cplx:
+            h1e = h1e + 1j * scipy.sparse.random(nmo, nmo).toarray()
+
+        rvs = scipy.stats.norm(scale=0.01).rvs
+        eri = scipy.sparse.random(nmo**2, nmo**2, data_rvs=rvs).toarray()
+        if cplx:
+            eri = eri + 1j * scipy.sparse.random(nmo**2, nmo**2, data_rvs=rvs).toarray()
+        eri = eri.reshape((nmo, nmo, nmo, nmo))
+
+    else:
+        h1e = numpy.random.random((nmo, nmo))
+        if cplx:
+            h1e = h1e + 1j * numpy.random.random((nmo, nmo))
+
+        eri = numpy.random.normal(scale=0.01, size=(nmo, nmo, nmo, nmo))
+        if cplx:
+            eri = eri + 1j * numpy.random.normal(scale=0.01, size=(nmo, nmo, nmo, nmo))
+
     # Restore symmetry to the integrals.
     if sym >= 4:
         # (ik|jl) = (jl|ik)