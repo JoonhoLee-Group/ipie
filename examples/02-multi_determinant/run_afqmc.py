05293c77506c450fafd6445ef8df92ca23824ba6
Fionn Malone <fmalone@google.com>
2023-12-10 00:48:12 +0000

    Fix import.

--- examples/02-multi_determinant/run_afqmc.py
+++ examples/02-multi_determinant/run_afqmc.py
@@ -1,12 +1,12 @@
+import h5py
 import numpy
-from mpi4py import MPI
+
+from ipie.config import MPI
+from ipie.hamiltonians.generic import Generic as HamGeneric
 from ipie.qmc.afqmc import AFQMC
 from ipie.systems.generic import Generic
-from ipie.hamiltonians.generic import Generic as HamGeneric
-import h5py
-from ipie.utils.from_pyscf import gen_ipie_input_from_pyscf_chk
 from ipie.trial_wavefunction.particle_hole import ParticleHoleNonChunked
-import scf
+from ipie.utils.from_pyscf import gen_ipie_input_from_pyscf_chk
 
 comm = MPI.COMM_WORLD
 gen_ipie_input_from_pyscf_chk("scf.chk", mcscf=True)
@@ -24,20 +24,17 @@ num_chol = chol.shape[0]
 ham = HamGeneric(
     numpy.array([h1e, h1e]),
     chol.transpose((1, 2, 0)).reshape((num_basis * num_basis, num_chol)),
-    e0
+    e0,
 )
 
 # Build trial wavefunction
-with h5py.File("wavefunction.h5", 'r') as fh5:
-    coeff = fh5['ci_coeffs'][:]
-    occa = fh5['occ_alpha'][:]
+with h5py.File("wavefunction.h5", "r") as fh5:
+    coeff = fh5["ci_coeffs"][:]
+    occa = fh5["occ_alpha"][:]
     occb = fh5["occ_beta"][:]
 wavefunction = (coeff, occa, occb)
 trial = ParticleHoleNonChunked(
-    wavefunction,
-    mol_nelec,
-    num_basis,
-    num_dets_for_props=len(wavefunction[0])
+    wavefunction, mol_nelec, num_basis, num_dets_for_props=len(wavefunction[0])
 )
 trial.compute_trial_energy = False
 trial.build()
@@ -53,8 +50,7 @@ afqmc_msd = AFQMC.build(
     num_blocks=10,
     timestep=0.005,
     stabilize_freq=5,
-    pop_control_freq=5
+    pop_control_freq=5,
 )
 afqmc_msd.run()
 afqmc_msd.finalise(verbose=True)
-
