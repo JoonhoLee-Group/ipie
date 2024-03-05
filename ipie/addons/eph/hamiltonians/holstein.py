ccc26493eb1913ffbe07752a43c3cbe556d1b811
m-baumgarten <mbaumgarten@boslogin02.rc.fas.harvard.edu>
2024-03-05 16:31:52 -0500

    Cleanup of HolsteinModel class plus some explanation of the model.

--- ipie/addons/eph/hamiltonians/holstein.py
+++ ipie/addons/eph/hamiltonians/holstein.py
@@ -1,10 +1,35 @@
 import numpy
-from ipie.hamiltonians.generic_base import GenericBase
 from ipie.utils.backend import arraylib as xp
 
 
-class HolsteinModel(GenericBase):
-    """Class for Holstein model carrying elph tensor and system parameters
+class HolsteinModel():
+    r"""Class carrying parameters specifying a 1D Holstein chain.
+
+    The Holstein model is described by the Hamiltonian
+
+    .. math::
+        \hat{H} = -t \sum_{\langle ij\rangle} \hat{a}_i^\dagger \hat{a}_j 
+        - g \sqrt{2 w_0 m} \sum_i \hat{a}_i^\dagger \hat{a}_i \hat{X}_i
+        + \bigg(\sum_i \frac{m w_0^2}{2} \hat{X}_i^2 + \frac{1}{2m} \hat{P}_i^2 
+        - \frac{w_0}{2}\bigg),
+
+    where :math:`t` is associated with the electronic hopping, :math:`g` with
+    the electron-phonon coupling strength, and :math:``w_0` with the phonon 
+    frequency. 
+
+    Parameters
+    ----------
+    g : 
+        Electron-phonon coupling strength
+    t : 
+        Electron hopping parameter
+    w0 : 
+        Phonon frequency
+    nsites : 
+        Length of the 1D Holstein chain
+    pbc : 
+        Boolean specifying whether periodic boundary conditions should be 
+        employed.
     """
 
     def __init__(self, g: float, t: float, w0: float, nsites: int, pbc: bool):
@@ -18,6 +43,7 @@ class HolsteinModel(GenericBase):
         self.const = -self.g * numpy.sqrt(2. * self.m * self.w0)
 
     def build(self):
+        """Constructs electronic hopping matrix."""
         self.T = numpy.diag(numpy.ones(self.nsites-1), 1)
         self.T += numpy.diag(numpy.ones(self.nsites-1), -1)
     
