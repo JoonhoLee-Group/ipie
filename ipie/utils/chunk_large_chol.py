3e24ed17fb266f4bb68e5412d5efa3a1cad8b362
jiangtong1000 <tongjiang@g.harvard.edu>
2024-04-08 17:28:27 -0400

    bug fix of split chol utils

--- ipie/utils/chunk_large_chol.py
+++ ipie/utils/chunk_large_chol.py
@@ -7,7 +7,7 @@ def split_cholesky(ham_filename, nmembers):
     #
     with h5py.File(ham_filename, "r") as source_file:
         dataset = source_file["LXmn"][()]  # pylint: disable=no-member
-        num_chol = dataset.shape[-1]
+        num_chol = dataset.shape[0]
     split_sizes, displacements = make_splits_displacements(num_chol, nmembers)
     assert isinstance(dataset, np.ndarray), "Dataset should be a numpy array"
     dataset = dataset.transpose(1, 2, 0).reshape(-1, num_chol)  # pylint: disable=no-member
