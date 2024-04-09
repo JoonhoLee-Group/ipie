575fbf9bf1c22bfa249c4edec65d41b673664150
jiangtong1000 <tongjiang@g.harvard.edu>
2024-04-08 20:21:57 -0400

    add unit test for chunking func

--- ipie/utils/chunk_large_chol.py
+++ ipie/utils/chunk_large_chol.py
@@ -3,8 +3,20 @@ import h5py
 import numpy as np
 
 
-def split_cholesky(ham_filename, nmembers):
-    #
+def split_cholesky(ham_filename: str, nmembers: int, verbose=True):
+    """
+    This function calculates the splits and displacements needed to distribute the
+    Cholesky vectors among the members and  splits the Cholesky decomposed Hamiltonian
+    vectors stored in an HDF5 file among a given number of members
+    (e.g., GPU cards to distribute total cholesky)
+
+    Parameters
+    ----------
+    ham_filename : str
+        The filename of the HDF5 file containing the total Cholesky (naux, nbas, nbas)
+    nmembers : int
+        The number of members among which the Cholesky vectors will be distributed.
+    """
     with h5py.File(ham_filename, "r") as source_file:
         dataset = source_file["LXmn"][()]  # pylint: disable=no-member
         num_chol = dataset.shape[0]
@@ -13,12 +25,13 @@ def split_cholesky(ham_filename, nmembers):
     dataset = dataset.transpose(1, 2, 0).reshape(-1, num_chol)  # pylint: disable=no-member
 
     for i, (size, displacement) in enumerate(zip(split_sizes, displacements)):
-        print(i, size, displacement)
+        # Prepare row indices for slicing
         row_start = displacement
         row_end = displacement + size
-
         with h5py.File(f"chol_{i}.h5", "w") as target_file:
             target_file.create_dataset("chol", data=dataset[:, row_start:row_end])
+        if verbose:
+            print(f"# Split {i}: Size {size}, Displacement {displacement}")
 
-    # Done
-    print("Splitting complete.")
+    if verbose:
+        print("# Splitting complete.")
