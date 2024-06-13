e7fc8775545591bee0a13ff603570378a2301c91
Jinghong Zhang <formic@dhcp-10-250-150-216.harvard.edu>
2024-06-13 13:49:48 -0400

    modified the author in the license

--- ipie/addons/adafqmc/estimators/__init__.py
+++ ipie/addons/adafqmc/estimators/__init__.py
@@ -12,6 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Authors: Fionn Malone <fmalone@google.com>
-#          Joonho Lee
+# Authors: Jinghong Zhang <jinghongzhang@fas.harvard.edu>
 #
