fb6958448eaccfffddff4800c15b407f99a093d9
Moritz Baumgarten <mbaumgarten@fas.harvard.edu>
2024-09-07 13:51:23 -0400

    fixed example

--- examples/19-1d_holstein/run_afqmc.py
+++ examples/19-1d_holstein/run_afqmc.py
@@ -21,6 +21,7 @@ from ipie.qmc.afqmc import AFQMC
 from ipie.systems import Generic
 from ipie.addons.eph.hamiltonians.holstein import HolsteinModel
 from ipie.addons.eph.trial_wavefunction.toyozawa import ToyozawaTrial
+
 # from ipie.addons.eph.trial_wavefunction.variational.toyozawa_variational import (
 #     variational_trial_toyozawa,
 # )
@@ -28,6 +29,7 @@ from ipie.addons.eph.walkers.eph_walkers import EPhWalkers
 from ipie.addons.eph.estimators.energy import EnergyEstimator
 from ipie.propagation.propagator import Propagator
 from ipie.addons.eph.propagation.holstein import HolsteinPropagator
+
 Propagator.update({HolsteinModel: HolsteinPropagator})
 
 # System Parameters
@@ -60,12 +62,17 @@ ham.build()
 #     initial_phonons, initial_electron, ham, system
 # )
 # wavefunction = np.column_stack([beta_shift, el_trial])
-wavefunction = np.load('wavefunction.npy')
+wavefunction = np.array(
+    [
+        [-0.05171059, 0.18699386, 0.21710474, 0.14657979, 0.29875713],
+        [1.07606477, 0.28428281, 1.00976995, -0.10386843, 1.45999226],
+        [0.34467794, 0.39093646, 0.40150302, 0.33165806, 0.54705797],
+        [1.45939397, 1.13142463, -0.14783494, 1.58959742, -0.35513289],
+    ]
+)
 
 # Setup trial
-trial = ToyozawaTrial(
-    wavefunction=wavefunction, w0=ham.w0, num_elec=[nup, ndown], num_basis=nsites
-)
+trial = ToyozawaTrial(wavefunction=wavefunction, w0=ham.w0, num_elec=[nup, ndown], num_basis=nsites)
 trial.set_etrial(ham)
 
 # Setup walkers
