97c79664ee74cf2b982815d8e9ee0394d050fbb3
victor2000chen-glitch <victor.2000.chen@gmail.com>
2026-05-13 16:09:58 +0200

    Add files via upload

---
+++ ipie/addons/ithc/qmc/comm.py
@@ -0,0 +1,143 @@
+# Copyright 2022 The ipie Developers. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Fionn Malone <fionn.malone@gmail.com>
+#          Joonho Lee
+#
+
+from dataclasses import dataclass
+from typing import TypeVar
+
+
+class FakeComm:
+    """Fake MPI communicator class to reduce logic."""
+
+    def __init__(self):
+        self.rank = 0
+        self.size = 1
+        self.buffer = {}
+
+    def Barrier(self):
+        pass
+
+    def barrier(self):
+        pass
+
+    def Get_rank(self):
+        return 0
+
+    def Get_size(self):
+        return 1
+
+    def Gather(self, sendbuf, recvbuf, root=0):
+        recvbuf[:] = sendbuf
+
+    def gather(self, sendbuf, root=0):
+        return [sendbuf]
+
+    def Allgather(self, sendbuf, recvbuf, root=0):
+        recvbuf[:] = sendbuf
+
+    def Bcast(self, sendbuf, root=0):
+        return sendbuf
+
+    def bcast(self, sendbuf, root=0):
+        return sendbuf
+
+    def isend(self, sendbuf, dest=None, tag=None):
+        return FakeReq()
+
+    def Isend(self, sendbuf, dest=None, tag=None):
+        self.buffer[tag] = sendbuf
+        return FakeReq()
+
+    def recv(self, source=None, root=0):
+        pass
+
+    def Recv(self, recvbuff, source=None, root=0, tag=0):
+        if self.buffer.get(tag) is not None:
+            recvbuff[:] = self.buffer[tag].copy()
+
+    def Allreduce(self, sendbuf, recvbuf, root=0):
+        recvbuf[:] = sendbuf
+
+    def allreduce(self, sendbuf, op=None, root=0):
+        return sendbuf.copy()
+
+    def Split(self, color: int = 0, key: int = 0):
+        return self
+
+    def Reduce(self, sendbuf, recvbuf, op=None, root=0):
+        recvbuf[:] = sendbuf
+
+    def Scatter(self, sendbuf, recvbuf, root=0):
+        recvbuf[:] = sendbuf
+
+    def Scatterv(self, sendbuf, recvbuf, root=0):
+        recvbuf[:] = sendbuf
+
+    def scatter(self, sendbuf, root=0):
+        assert sendbuf.shape[0] == 1, "Incorrect array shape in FakeComm.scatter"
+        return sendbuf[0]
+
+    def Issend(self, sendbuf, dest=None, tag=None):
+        # For size=1, just store by tag
+        self.buffer[tag] = sendbuf.copy() if hasattr(sendbuf, "copy") else sendbuf
+        return FakeReq()
+
+    def Irecv(self, recvbuf, source=None, tag=None):
+        def _do_copy():
+            if tag in self.buffer:
+                recvbuf[:] = self.buffer[tag].copy()
+
+        return FakeReq(action=_do_copy)
+
+
+class FakeStatus:
+    pass
+
+
+class FakeReq:
+    def __init__(self, action=None):
+        self._action = action
+
+    def wait(self):
+        if self._action is not None:
+            self._action()
+
+    def Wait(self, status=None):
+        self.wait()
+
+
+@dataclass
+class MPI:
+    COMM_WORLD = FakeComm()
+    SUM = None
+    COMM_SPLIT_TYPE_SHARED = None
+    COMM_TYPE_SHARED = None
+    DOUBLE = None
+    INT64_T = None
+    Win = None
+    IntraComm = TypeVar("IntraComm")
+
+    Status = FakeStatus
+
+    class Request:
+        @staticmethod
+        def Waitall(reqs):
+            for r in reqs:
+                if hasattr(r, "Wait"):
+                    r.Wait()
+                elif hasattr(r, "wait"):
+                    r.wait()
---
+++ ipie/addons/ithc/qmc/utils.py
@@ -0,0 +1,40 @@
+# Copyright 2022 The ipie Developers. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Fionn Malone <fmalone@google.com>
+#          Joonho Lee
+#
+
+import numpy
+
+from ipie.utils.backend import arraylib as xp
+
+
+def set_rng_seed(seed, comm):
+    if seed is None:
+        # only set "random" part of seed on parent processor so we can reproduce
+        # results in when running in parallel.
+        if comm.rank == 0:
+            seed = numpy.array([numpy.random.randint(0, 1e8)], dtype="i4")
+            # Can't directly json serialise numpy arrays
+        else:
+            seed = numpy.empty(1, dtype="i4")
+        comm.Bcast(seed, root=0)
+        seed = seed[0]
+    seed = seed + comm.rank
+    xp.random.seed(seed)
+    if comm.rank == 0:
+        print(f"# random seed is {seed}")
+    # assert False, f"{seed} {comm.rank}"
+    return seed
