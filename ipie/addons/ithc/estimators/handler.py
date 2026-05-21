e5edfc67922289037bc7da7e303de8ba506b009b
Victor Chen <victor.2000.chen@gmail.com>
2026-05-21 11:39:50 +0200

    Update handler.py to include additional authors
    Added authorship information for Maxine Luo and Victor Chen.

--- ipie/addons/ithc/estimators/handler.py
+++ ipie/addons/ithc/estimators/handler.py
@@ -14,6 +14,8 @@
 #
 # Authors: Fionn Malone <fmalone@google.com>
 #          Joonho Lee
+#          Maxine Luo <man.luo@mpq.mpg.de>
+#          Victor Chen <victor.chen@tum.de>
 #
 
 """Routines and classes for estimation of observables."""
